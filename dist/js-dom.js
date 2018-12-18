877b6293862d88ad53d7fdebb903cfe058f3d6d3
yama-dev <tatsuya.yamamoto69@gmail.com>
2018-12-18 14:14:21 +0900

    update version text.

--- dist/js-dom.js
+++ dist/js-dom.js
@@ -2,7 +2,7 @@
 /*!
  * JS DOM (JavaScript Library)
  *   js-dom.js
- * Version 0.0.2
+ * Version 0.0.3
  * Repository https://github.com/yama-dev/js-dom
  * Copyright yama-dev
  * Licensed under the MIT license.

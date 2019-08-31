2acafb2e531c08aee6a1fc49619c2506d48b2ca6
yama-dev <tatsuya.yamamoto69@gmail.com>
2019-08-31 19:25:30 +0900

    adjust build setting.

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,74 @@
+const pkg = require('./package.json');
+
+const comment = `JS DOM (JavaScript Library)
+  ${pkg.name}
+Version ${pkg.version}
+Repository ${pkg.repository.url}
+Copyright ${pkg.author}
+Licensed ${pkg.license}`;
+
 const env = process.env.NODE_ENV;
 
 const webpack = require('webpack');
 
+const webpackPlugEnv = new webpack.EnvironmentPlugin({
+  NODE_ENV: env,
+  VERSION: pkg.version,
+  DEBUG: false
+});
+
+const webpackPlugBnr = new webpack.BannerPlugin({
+  banner: comment,
+});
+
+const babelPlugin = [
+  '@babel/plugin-transform-object-assign'
+];
+
 const config = {
   mode: env || 'development',
-  entry: './src/js-dom.js',
+  entry: {
+    'js-dom': './src/js-dom.js'
+  },
   output: {
     path: `${__dirname}/dist`,
-    filename: 'js-dom.js',
-    library: 'JS_DOM',
-    libraryExport: 'default',
+    filename: '[name].js',
     libraryTarget: 'umd'
   },
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.(js)$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
       },
       {
         test: /\.js$/,
+        exclude: /node_modules[//\/](?!(@yama\-dev)\/).*/,
         use: [
           {
             loader: 'babel-loader',
             options: {
               presets: [
-                ['env', {'modules': false}]
-              ]
+                [
+                  '@babel/preset-env',
+                  {
+                    modules: false
+                  }
+                ]
+              ],
+              plugins: babelPlugin
             }
           }
         ],
-        exclude: /node_modules/,
       }
     ]
-  }
+  },
+  plugins: [
+    webpackPlugEnv,
+    webpackPlugBnr
+  ]
 };
 
 module.exports = config;
